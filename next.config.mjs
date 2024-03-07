61f5ae025404d6ad17e67e397a627a925792d435
Oscar Nilsson <Oscar.Nilsson2@nexergroup.com>
2024-03-07 15:41:54 +0100

    Added config for including web.config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+// next.config.js
 const nextConfig = {
-    output: 'next-azure'
-};
+    webpack: (config, { isServer }) => {
+        // Include the web.config file
+        config.plugins.push(
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: 'web.config', 
+                        to: '', 
+                    },
+                ],
+            })
+        );
 
+        return config;
+    },
+};
 export default nextConfig;
