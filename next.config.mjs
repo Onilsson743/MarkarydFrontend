cb22d0f9694e0fc4c07d89cc395a6bec38dddda8
Oscar Nilsson <Oscar.Nilsson2@nexergroup.com>
2024-03-11 13:31:50 +0100

    Added docker files

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
-import CopyWebpackPlugin from 'copy-webpack-plugin';
 // next.config.js
 const nextConfig = {
-    webpack: (config, { isServer }) => {
-        // Include the web.config file
-        config.plugins.push(
-            new CopyWebpackPlugin({
-                patterns: [
-                    {
-                        from: 'web.config', 
-                        to: '', 
-                    },
-                ],
-            })
-        );
-
-        return config;
-    },
+    output: 'standalone'
 };
 export default nextConfig;
